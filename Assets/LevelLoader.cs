b6cbc7d323dfcbffe77ec5f6fa6ea9730e3b0acb
Vitor Taichi Taira <vitortaira@alunos.utfpr.edu.br>
2023-12-06 14:50:19 -0300

    att + transição

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitiontime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            LoadNextLevel();
+        }
+    }
+    public void LoadNextLevel(){
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //play animation
+        transition.SetTrigger("Start");
+        //wait
+        yield return new WaitForSeconds(transitiontime);
+        //load scane
+        SceneManager.LoadScene(levelIndex);
+    }
+}
